9213a60b62501c1fdb85b091729eb05b0c9c4baa
Abhipande0104 <123271240+Abhipande0104@users.noreply.github.com>
2023-05-09 23:25:36 +0530

    Update Quiz2.java

--- dayy11_StringQuiz1/Quiz2.java
+++ dayy11_StringQuiz1/Quiz2.java
@@ -4,11 +4,12 @@ public class Quiz2
 {
 	public static void main(String[] args)
 	{
-		String str ="core";
+		String str ="core2";
 		str.concat("web");
 		System.out.println(str);
 	}
 }
 
-//o/p= core
+//o/p= core2
 //for change we need to reassign as str=str.concat("web")
+//concat() method belongs to String class
